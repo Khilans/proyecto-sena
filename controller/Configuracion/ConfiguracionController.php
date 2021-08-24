79c5a4fc903a3ddd250c8c1a793f845af46aa701
Sebastian <jsrodriguez3032@misena.edu.co>
2021-08-23 22:02:37 -0500

    Ajuste por realizar en el controlador Acceso

---
+++ controller/Configuracion/ConfiguracionController.php
@@ -0,0 +1,6 @@
+<?php
+    include_once '../model/Configuracion/ConfiguracionModel.php';
+    class ConfiguracionController{
+        function 
+    }
+?>
--- web/login.php
+++ web/login.php
@@ -4,9 +4,9 @@
 ?>
 <form action="<?php echo getUrl("Acceso","Acceso","login"); ?>" method="post">
    <label>Número de documento</label>
-    <input type="number" name="n_documento" placeholder="Digite número de documento">
+    <input type="number" name="usu_ndocumento" placeholder="Digite número de documento">
     <label>Contraseña</label>
-    <input type="password" name="contraseña" id="">
+    <input type="password" name="usu_pass" id="">
     <input type="submit" value="Enviar">
 </form>
 
