80ec320fbbfeae1a9dbe1da30b54b1b3b3e22a5e
Sebastian <jsrodriguez3032@misena.edu.co>
2021-08-24 09:36:06 -0500

    Añadido modal para editar usuarios

--- controller/Usuarios/UsuariosController.php
+++ controller/Usuarios/UsuariosController.php
@@ -5,7 +5,7 @@
 
         public function consult(){
             $obj=new UsuariosModel;
-            $sql="SELECT usu_nombre, usu_nombre2, usu_apellido, cod_tipo_doc,usu_ndocumento, usu_correo FROM t_usuario WHERE id_estado=1";
+            $sql="SELECT u.usu_nombre, u.usu_id, u.usu_nombre2, u.usu_apellido, td.nom_tipo_doc, u.usu_ndocumento, u.usu_correo FROM t_usuario u, t_tipodocumento td WHERE id_estado=1 AND td.cod_tipo_doc=u.cod_tipo_doc ";
             $usuarios=$obj->consult($sql);
             include_once '../view/usuarios/consult.php';
         }
@@ -15,14 +15,18 @@
             $id=$_SESSION['user_id'];
             $sql="SELECT * FROM t_usuario WHERE usu_id=$id";
             $usuario=$obj->consult($sql);
-            include_once '../view/usuarios/profile.php';
+            include_once '../view/usuarios/Modal.php';
         }
 
-        public function getUpdateProfile(){
-
+        public function getUpdateModal(){
+            $obj=new UsuariosModel;
+            $id=$_SESSION['user_id'];
+            $sql="SELECT usu_nombre, usu_id, usu_nombre2, usu_apellido, usu_correo, cod_tipo_doc, usu_ndocumento, cod_rol FROM t_usuario WHERE usu_id=$id";
+            $usuarios=$obj->consult($sql);
+            include_once '../view/usuarios/modalUpdate.php';
         }
 
-        public function postUpdateProfile(){
+        public function postUpdateModal(){
 
         }
 
