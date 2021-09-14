631f609cf509013d0fbd67cd06158e808622d00f
amshi <acsantacruz7@misena.edu.co>
2021-09-13 21:30:42 -0500

    Modales

--- admin/view/Noticia/ModalDelete.php
+++ admin/view/Noticia/ModalDelete.php
@@ -1,25 +1,29 @@
-<div class="jumbotron mt-4">
-    <h3 class="display-4">Eliminar noticia</h3>
+<div class="container">
+    <div class="alert alert-danger" role="alert">
+            <h4 class="alert-heading text-center">¿Está seguro de realizar esta acción?</h4>
+            <p>La noticia será inhabilitada indefinidamente</p>
+    </div>        
+    <?php
+        foreach ($noticias as $not) {
+    ?>
+    <form action="<?php echo getUrl("Noticia","Noticia","postDelete"); ?>" method="post">
+
+        <div style="margin: 1em;" class="row">
+            <div class="form-group col-md-4">
+                <label>Nombre</label>
+                <input type="hidden" name="cod_noticia" value="<?php echo $not['cod_noticia']; ?>">
+                <input type="text" readonly name="titulo_noticia" class="form-control" value="<?php echo $not['titulo_noticia']; ?>"> 
+            
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6">
+                <input style="margin: 1em;" type="submit" value="Eliminar" class="btn btn-danger">
+                <a href="<?php echo getUrl("Noticia","Noticia","consult") ?>"><button type="button" class="btn btn-success">Cancelar</button></a>
+            </div>
+        </div>
+    </form>
 </div>
-<?php
-    foreach ($noticias as $not) {
-?>
-<form action="<?php echo getUrl("Noticia","Noticia","postDelete"); ?>" method="post">
-    <div class="row">
-        <div class="form-group col-md-4">
-            <label>Nombre</label>
-            <input type="hidden" name="cod_noticia" value="<?php echo $not['cod_noticia']; ?>">
-            <input type="text" readonly name="titulo_noticia" class="form-control" value="<?php echo $not['titulo_noticia']; ?>"> 
-            <p class="mt-4">¿Esta seguro de eliminar esta noticia?</p>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-md-4">
-            <input type="submit" value="Eliminar" class="btn btn-danger">
-            <a href="<?php echo getUrl("Noticia","Noticia","consult") ?>"><button type="button" class="btn btn-success">Cancelar</button></a>
-        </div>
-    </div>
-</form>
 <?php
     }
 ?>
---
+++ admin/view/Noticia/ModalInsert.php
@@ -0,0 +1,50 @@
+<div class="container">
+<div class="alert alert-success" role="alert">
+                <h4 class="alert-success text-center">Registrar noticia</h4>
+                <p></p>
+        </div>
+	<form action="<?php echo getUrl("Noticia", "Noticia", "postInsert"); ?>" method="post" enctype="multipart/form-data">
+		<div style="margin: 1em;" class="row">
+			<div class="col-md-7 form-group">
+				<label>Descripción</label>
+				<textarea name="desc_noticia" rows="1" cols="50" placeholder="Descripcion de la noticia"></textarea>
+			</div>
+			<div class="col-md-7 form-group">
+				<label>Título</label>
+				<input type="text" name="titulo_noticia" class="form-control" placeholder="Titulo">
+			</div>
+			<div class="col-md-7 form-group">
+				<label>Fecha</label>
+				<input type="date" name="fecha_noticia" class="form-control" placeholder="Fecha">
+			</div>
+			<div class="col-md-7">
+				<label>Imagen</label>
+				<input type="file" name="img_noticia">
+			</div>
+			<div class="col-md-7 form-group">
+				<label>Tipo de noticia</label>
+				<select name="cod_tipo_noti" class="form-control">
+					<option value="">Seleccione...</option>
+
+					<?php
+					foreach ($tipo_noticias as $tipo_not) {
+						echo "<option value='" . $tipo_not['cod_tipo_noti'] . "'>" . $tipo_not['desc_tipo_noti'] . "</option>";
+					}
+					?>
+				</select>
+			</div>
+			<div class="col-md-7 form-group">
+				<label></label>
+				<input type="hidden" name="id_estado" class="form-control" >
+			</div>
+
+		</div>
+		<div class="row">
+			<div style="margin: 1em;" class="col-md-6">
+				<input type="submit" value="Enviar" class="btn btn-success">
+				<a href="<?php echo getUrl("Noticia", "Noticia", "consult"); ?>"><button type="button" class="btn btn-primary">Volver</button></a>
+			</div>
+		</div>
+
+
+	</form>
