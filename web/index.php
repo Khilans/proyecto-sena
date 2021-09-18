643cd050462193d00d7fcb4b1b19c8de62830d46
amshi <acsantacruz7@misena.edu.co>
2021-09-18 13:20:04 -0500

    PQRSF

--- web/index.php
+++ web/index.php
@@ -14,7 +14,13 @@ include_once '../lib/helpers.php';
                     include_once '../view/partials/register.php';
                     
                 }
+                
+                /* if (!isset($_SESSION['user_id'])){
+                    include_once '../view/Pqrs/sendU.php'; 
+                } */
+
                 /*  include_once '../view/partials/footer.php';  */
+                
             echo "</div>";
             include_once '../view/partials/scripts.php';
         echo "</body>";
