1b9b448f07624602755ce9be16ce4910110737b3
amshi <acsantacruz7@misena.edu.co>
2021-09-20 13:58:13 -0500

    Obligada a hacer commit

--- usu/view/partials/home.php
+++ usu/view/partials/home.php
@@ -1,34 +1,33 @@
-	<!-- Home -->
+<!-- Home -->
 
-	<div class="home">
+<div class="home">
 
-		<!-- Hero Slider -->
-		<div class="hero_slider_container">
-			<div class="hero_slider owl-carousel">
-			
-				
-				<!-- Hero Slide -->
-				<div class="hero_slide">
-					<div class="hero_slide_background" style="background-image:url(images/tele.jpg)"></div>
-					<div class="hero_slide_container d-flex flex-column align-items-center justify-content-center">
-						<div class="hero_slide_content text-center">
-						<h1 data-animation-in="fadeInUp" data-animation-out="animate-out fadeOut">Un mundo de<br>Telecomunicaciones</h1>
+<!-- Hero Slider -->
+<div class="hero_slider_container">
+	<div class="hero_slider owl-carousel">
+	
+		
+		<!-- Hero Slide -->
+		<div class="hero_slide">
+			<div class="hero_slide_background" style="background-image:url(images/tele.jpg)"></div>
+			<div class="hero_slide_container d-flex flex-column align-items-center justify-content-center">
+				<div class="hero_slide_content text-center">
+				<h1 data-animation-in="fadeInUp" data-animation-out="animate-out fadeOut">Un mundo de<br>Telecomunicaciones</h1>
 
-						</div>
-					</div>
 				</div>
-				<div class="hero_slide">
-					<div class="hero_slide_background" style="background-image:url(images/tele1.jpg)"></div>
-					<div class="hero_slide_container d-flex flex-column align-items-center justify-content-center">
-						<div class="hero_slide_content text-center">
-						
-						</div>
-					</div>
-				</div>
-
 			</div>
-
+		</div>
+		<div class="hero_slide">
+			<div class="hero_slide_background" style="background-image:url(images/tele1.jpg)"></div>
+			<div class="hero_slide_container d-flex flex-column align-items-center justify-content-center">
+				<div class="hero_slide_content text-center">
+				
+				</div>
+			</div>
 		</div>
 
 	</div>
-    
+
+</div>
+
+</div>
