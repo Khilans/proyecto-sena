d3911c6fb8eed2fc96cedca40cb0d4e20cc8cddb
Natsugame <jjmarin465@misena.edu.co>
2021-09-21 12:13:30 -0500

    cambio en index

---
+++ usu/view/partials/register.php
@@ -0,0 +1,40 @@
+	<!-- Register -->
+
+	<div class="register">
+
+		<div class="container-fluid">
+			
+			<div class="row row-eq-height">
+				<div class="col-lg-6 nopadding">
+					
+					<!-- Register -->
+
+					<div class="register_section d-flex flex-column align-items-center justify-content-center">
+						<div class="register_content text-center">
+						<center>
+						<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3982.6684695040644!2d-76.49362068524188!3d3.430634397502953!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e30a71a83f469d3%3A0xeb602ca40963938!2sSena%20Pondaje!5e0!3m2!1ses-419!2sco!4v1631251912479!5m2!1ses-419!2sco" width="500" height="500" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+					</center></div>
+					</div>
+ 
+				</div>
+
+				<div class="col-lg-6 nopadding">
+					
+					<!-- Search -->
+
+					<div class="search_section d-flex flex-column align-items-center justify-content-center">
+						<div class="search_background" style="background-image:url(images/index.jpg);"></div>
+						<div class="search_content text-center">
+							<h1 class="search_title">Search for your course</h1>
+							<h1 class="search_title">Register now and get a discount <span>50%</span> discount until 1 January</h1>
+							<p class="search_title">In aliquam, augue a gravida rutrum, ante nisl fermentum nulla, vitae tempor nisl ligula vel nunc. Proin quis mi malesuada, finibus tortor fermentum. Aliquam, augue a gravida rutrum, ante nisl fermentum nulla, vitae tempo.</p>
+							<div class="button button_1 register_button mx-auto trans_200"><a href="#">register now</a></div>
+						
+						</div> 
+					</div>
+
+				</div>
+			</div>
+		</div>
+	</div>
+	
