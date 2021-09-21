0f9e0df4762a5de555b4eceb5f2e6631e5948a43
Natsugame <jjmarin465@misena.edu.co>
2021-09-21 15:19:57 -0500

    cambio noticias

--- usu/view/Noticia/noticia.php
+++ usu/view/Noticia/noticia.php
@@ -3,32 +3,35 @@
 
 
 <div class="home">
-  <img src="images/teachers_background.jpg" class="img-fluid" alt="...">
-
-  <div class="col mt-2">
-    <p class="h1 text-info">Noticias</p>
-    <div class="p-1 mb-2 bg-warning"></div>
-  </div>
+	<div class="home_background_container prlx_parent">
+		<div class="home_background prlx" style="background-image:url(images/noticias.jpg)"></div>
+	</div>
+	<div class="home_content">
+		<h1>NOTICIAS</h1>
+	</div>
 </div>
+
 
 <?php
 foreach ($noticias as $noti) {
 ?>
-  <div style="margin: 2em; padding-bottom: 5px" class="col-md-10">
+  <div style="margin: 1em; padding-bottom: 5px" class="col-md-10">
     <div class="caja">
       <div style="width: 17rem;">
-        <img src="<?php echo $noti['img_noticia'] ?>" class="img-thumbnail" alt="Error al cargar la imagen">
+        <img src="<?php echo $noti['img_noticia'] ?>" class="img-thumbnail" alt="Error al cargar la imagen" high="212">
       </div>
       <div class="card-body">
         <div class="meta col">
           <time class="published" datetime="2015-11-01"><?php echo $noti['fecha_noticia'] ?></time>
-          <h5 class=" card-title"><?php echo $noti['titulo_noticia'] ?></h5>
+          <h2 class=" card-title"><?php echo $noti['titulo_noticia'] ?></h2>
           <p class=" card-text"><?php echo $noti['desc_noticia'] ?></p>
           <a href="<?php echo getUrl("Noticia", "Noticia", "getDetalleNoticia", array("cod_noticia" => $noti['cod_noticia']))  ?>">Seguir leyendo...</a>
         </div>
       </div>
     </div>
   </div>
+
+
 
 <?php
 }
